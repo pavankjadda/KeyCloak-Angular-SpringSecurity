9124c2371d4f87187ca033cfa78e1cd3e2bb70c7
Pavan Jadda <>
2019-07-02 14:07:17 -0400

    first commit

---
+++ src/main/java/com/pj/keycloak/KeyCloakSpringSecurityApplication.java
@@ -0,0 +1,15 @@
+package com.pj.keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyCloakSpringSecurityApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(KeyCloakSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pj/keycloak/KeyCloakSpringSecurityApplicationTests.java
@@ -0,0 +1,18 @@
+package com.pj.keycloak;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KeyCloakSpringSecurityApplicationTests
+{
+
+    @Test
+    public void contextLoads()
+    {
+    }
+
+}
